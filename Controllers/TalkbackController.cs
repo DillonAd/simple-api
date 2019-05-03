a9982aed742a1f883a6438f9e153a87c55e8c1ed
dillon <28228453+DillonAd@users.noreply.github.com>
2019-05-02 22:28:28 -0500

    Adding basic webapi project for demos

---
+++ Controllers/TalkbackController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace simple_api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TalkbackController : ControllerBase
+    {
+        private static Queue<string> _talkbackQueue = new Queue<string>();
+
+        [HttpGet("All")]
+        public ActionResult<IEnumerable<string>> GetAll()
+        {
+            var results = new List<string>();
+
+            while(_talkbackQueue.TryDequeue(out string result))
+            {
+                results.Add(result);
+            }
+
+            return results;
+        }
+
+        [HttpGet("Next")]
+        public ActionResult<string> GetNext()
+        {
+            if(!_talkbackQueue.TryDequeue(out string value))
+            {
+                value = string.Empty;
+            }
+
+            return value;
+        }
+
+        [HttpPost("New")]
+        public void Post(string value) => _talkbackQueue.Enqueue(value);
+        
+    }
+}
